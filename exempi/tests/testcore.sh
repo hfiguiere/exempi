2a343c7493dec668e61d98d89ef9dc8f906e0e77
Hubert Figuiere <hub@figuiere.net>
2007-08-12 22:52:48 -0400

    Exempi side of GIF Support (testcore + Makefiles)

--- exempi/tests/testcore.sh
+++ exempi/tests/testcore.sh
@@ -9,7 +9,7 @@ if [ -z $srcdir ] ; then
     exit 255
 fi
 
-SAMPLES="BlueSquare.ai   BlueSquare.eps   BlueSquare.jpg  BlueSquare.mp3  BlueSquare.png  BlueSquare.tif BlueSquare.avi  BlueSquare.indd BlueSquare.pdf  BlueSquare.psd  BlueSquare.wav"
+SAMPLES="BlueSquare.ai  BlueSquare.eps BlueSquare.gif  BlueSquare.jpg  BlueSquare.mp3  BlueSquare.png  BlueSquare.tif BlueSquare.avi  BlueSquare.indd BlueSquare.pdf  BlueSquare.psd  BlueSquare.wav"
 SAMPLES_DIR=$srcdir/../../samples/BlueSquares
 DUMPMAINXMP_PROG=../../samples/source/dumpmainxmp
 
