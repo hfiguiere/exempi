535aa9d7d0b605492e8b6e927244d77404dff025
Hubert Figuière <hub@figuiere.net>
2017-01-22 13:31:18 -0500

    2.4.x: Test for xmp_parse()
    Currently pass, but incorrect.
    See https://bugs.freedesktop.org/show_bug.cgi?id=99494

---
+++ exempi/tests/testparse.cpp
@@ -0,0 +1,78 @@
+/*
+ * exempi - test3.cpp
+ *
+ * Copyright (C) 2007-2017 Hubert Figuiere
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1 Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2 Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the
+ * distribution.
+ *
+ * 3 Neither the name of the Authors, nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software wit hout specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <string>
+#include <iostream>
+
+#include <boost/test/minimal.hpp>
+#include <boost/format.hpp>
+
+#include "utils.h"
+#include "xmp.h"
+#include "xmpconsts.h"
+
+using boost::unit_test::test_suite;
+
+// void test_exempi_iterate()
+int test_main(int argc, char *argv[])
+{
+  char *buffer;
+
+  buffer = strdup("This is not even XML");
+  size_t len = strlen(buffer);
+
+  BOOST_CHECK(len != 0);
+  BOOST_CHECK(xmp_init());
+
+  XmpPtr xmp = xmp_new_empty();
+
+  // TODO current xmp_parse will succeed with invalid XML
+  BOOST_CHECK(xmp_parse(xmp, buffer, len));
+
+  BOOST_CHECK(xmp != NULL);
+
+  BOOST_CHECK(xmp_free(xmp));
+
+  free(buffer);
+  xmp_terminate();
+
+  BOOST_CHECK(!g_lt->check_leaks());
+  BOOST_CHECK(!g_lt->check_errors());
+  return 0;
+}
