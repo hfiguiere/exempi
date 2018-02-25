d0b22edd422700942500f9949f30258b1abd6fde
Hubert Figuière <hub@figuiere.net>
2018-02-25 14:01:52 -0500

    2.4.x: Bug 105205 - fix a buffer overflow in the TIFF parser

--- XMPFiles/source/FileHandlers/TIFF_Handler.cpp
+++ XMPFiles/source/FileHandlers/TIFF_Handler.cpp
@@ -235,8 +235,9 @@ void TIFF_MetaHandler::ProcessXMP()
 			// zero padding for the IPTC digest. If the full digest differs, recheck without the padding.
 
 			iptcDigestState = PhotoDataUtils::CheckIPTCDigest ( iptcInfo.dataPtr, iptcInfo.dataLen, digestInfo.dataPtr );
-
-			if ( (iptcDigestState == kDigestDiffers) && (kTIFF_TypeSizes[iptcInfo.type] > 1) ) {
+			// See bug https://bugs.freedesktop.org/show_bug.cgi?id=105205
+			// if iptcInfo.dataLen is 0, then there is no digest.
+			if ( (iptcDigestState == kDigestDiffers) && (kTIFF_TypeSizes[iptcInfo.type] > 1) && iptcInfo.dataLen > 0 ) {
 				XMP_Uns8 * endPtr = (XMP_Uns8*)iptcInfo.dataPtr + iptcInfo.dataLen - 1;
 				XMP_Uns8 * minPtr = endPtr - kTIFF_TypeSizes[iptcInfo.type] + 1;
 				while ( (endPtr >= minPtr) && (*endPtr == 0) ) --endPtr;
